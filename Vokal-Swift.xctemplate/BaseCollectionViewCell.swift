875aec08dd8327dcb56f7ec976aa5a70049da136
Brock Boland <brock@brockboland.com>
2017-02-01 14:24:06 -0600

    Added base collection view cell

---
+++ Vokal-Swift.xctemplate/BaseCollectionViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell, CommonInitializedView {
+    
+    var oneTimeThingsAreSetUp = false
+    
+    //MARK: - Setup functions which should be overridden
+    
+    func commonInit() {
+        self.setupOneTimeThingsIfNeeded()
+    }
+    
+    func setupOneTimeThings() {
+        self.oneTimeThingsAreSetUp = true
+    }
+    
+    //MARK: - Initialization
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    //MARK: - Interface Builder
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.commonInit()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.commonInit()
+    }
+    
+}
