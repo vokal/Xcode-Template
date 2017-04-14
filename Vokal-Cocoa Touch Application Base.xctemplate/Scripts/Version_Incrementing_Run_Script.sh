9cd5a656dc59013b617c47547ab1365f08a4df3c
Bryan Luby <bluby74@gmail.com>
2017-04-14 15:58:58 -0500

    Update version incrementing run script
    Check that there actually is a settings bundle in case we need to use the run script on a target that doesn't have a settings bundle.

--- Vokal-Cocoa Touch Application Base.xctemplate/Scripts/Version_Incrementing_Run_Script.sh
+++ Vokal-Cocoa Touch Application Base.xctemplate/Scripts/Version_Incrementing_Run_Script.sh
@@ -3,7 +3,7 @@
 # Update the Info.plist build number with number of git commits
 buildNumber=$(git rev-list HEAD --count) \
     || exit 1
-echo "Updating build number to ${buildNumber}..."
+echo "note: Updating build number to ${buildNumber}..."
 
 # Update the Info.plist build number with number of git commits
 /usr/libexec/PlistBuddy \
@@ -21,16 +21,20 @@ if [[ -e "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Info.plist"
 fi
 
 # Update the Settings.bundle
-bundleShortVersionString=$(/usr/libexec/PlistBuddy \
-    -c "Print :CFBundleShortVersionString" \
-    "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}" \
-    ) \
-    || exit 1
-newVersion="${bundleShortVersionString} (${buildNumber})"
-echo "Setting version number in settings bundle to ${newVersion}..."
-/usr/libexec/PlistBuddy \
-    -c "Set PreferenceSpecifiers:1:DefaultValue ${newVersion}" \
-    "${TARGET_BUILD_DIR}"/"${PRODUCT_NAME}.app"/Settings.bundle/Root.plist \
-    || exit 1
+settingsBundlePlistPath="${TARGET_BUILD_DIR}/${PRODUCT_NAME}.app/Settings.bundle/Root.plist"
 
-echo "Done."
+if [[ -e "${settingsBundlePlistPath}" ]]; then
+    bundleShortVersionString=$(/usr/libexec/PlistBuddy \
+        -c "Print :CFBundleShortVersionString" \
+        "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}" \
+        ) \
+        || exit 1
+    newVersion="${bundleShortVersionString} (${buildNumber})"
+    echo "note: Setting version number in settings bundle to ${newVersion}..."
+    /usr/libexec/PlistBuddy \
+        -c "Set PreferenceSpecifiers:1:DefaultValue ${newVersion}" \
+        "${settingsBundlePlistPath}" \
+        || exit 1
+fi
+
+echo "note: Done."
